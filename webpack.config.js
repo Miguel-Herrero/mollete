5222983a4200bb00d8ae52aae6819c2741d35926
Miguel Herrero <miguelherrero@MacBook-de-Miguel.local>
2018-01-19 00:00:53 +0100

    Revert outut

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './mollete/dist'),
-    publicPath: '/mollete/',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/mollete/dist/',
     filename: 'build.js'
   },
   module: {
