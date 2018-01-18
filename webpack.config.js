480a73b37e5426d736647455e60acc65a68380ee
Miguel Herrero <miguelherrero@MacBook-de-Miguel.local>
2018-01-19 00:04:08 +0100

    Update paths for GitHub

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: './mollete/dist/',
+    publicPath: '/mollete/dist/',
     filename: 'build.js'
   },
   module: {
