638648e27059b2b9f6a2ab513576ef7affe13360
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-01-20 17:44:45 +0100

    Update

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-  apiKey: "AIzaSyB9rx9XlB9wDGVfyo-ZXj_vKtswL5vv0lY",
-  authDomain: "api-project-875720539301.firebaseapp.com",
-  databaseURL: "https://api-project-875720539301.firebaseio.com",
-  projectId: "api-project-875720539301",
-  storageBucket: "api-project-875720539301.appspot.com",
-  messagingSenderId: "875720539301"
-}
