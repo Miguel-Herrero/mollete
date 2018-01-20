c8c819fd6258bbf4c585208671ac103ff0250ebf
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2018-01-20 14:03:07 +0100

    Sync data

--- src/App.vue
+++ src/App.vue
@@ -29,22 +29,21 @@
 </template>
 
 <script>
-
+import db from './firebaseInit'
 import UsersList from './UsersList'
 import FoodList from './FoodList'
+
 export default {
   name: 'app',
+
   data () {
     return {
       selectedUser: '',
-      cart: {},
-      prop: {
-        'cafe-solo': 1,
-        'mollete': 1
-      }
+      cart: {}
     }
   },
-  methods: {
+
+methods: {
     selectUser(user) {
       this.selectedUser = user
     },
@@ -59,11 +58,20 @@ export default {
       } else {
         this.$set(this.cart[this.selectedUser], food, 1)
       }
+
+      this.updateFirestore()
     },
 
     deleteCartOfUser (user) {
       this.selectedUser = ''
       this.$delete(this.cart, user)
+
+      this.updateFirestore()
+    },
+
+    updateFirestore () {
+      const cartRef = db.collection('mollete-orders').doc(this.dateId)
+      cartRef.set(this.cart)
     }
   },
 
@@ -86,10 +94,34 @@ export default {
       })
 
       return order
+    },
+
+    /**
+     * Firestore key is current date in format YYYYMMDD
+     * @returns {String} Date in YYYYMMDD format
+     */
+    dateId () {
+      const d = new Date()
+      const year = d.getFullYear()
+      let month = d.getMonth() + 1
+      if (month.toString().length < 2) {
+        month = `0${month}`
+      }
+      let day = d.getDate()
+      if (day.toString().length < 2) {
+        day = `0${day}`
+      }
+
+      return `${year}${month}${day}`
     }
   },
   mounted () {
-    window.scrollTo(0, 1); 
+    // Query Firestore to have real-time updates of today's cart
+    db.collection('mollete-orders').doc(this.dateId).onSnapshot(doc => {
+      if (doc) {
+        this.cart = doc && doc.data()
+      }
+    })
   },
   components: {
     FoodList,
